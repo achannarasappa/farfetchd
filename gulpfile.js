17ee59702142bab0de5016b0a8e540b316ff1ac7
Ani Channarasappa <git@ani.dev>
2016-01-19 21:39:20 -0500

    Added bundle gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var mocha = require('gulp-mocha');
 var jscs = require('gulp-jscs');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var Karma = require('karma').Server;
 var spawn = require('child_process').spawn;
 var runSequence = require('run-sequence');
@@ -27,6 +33,24 @@ gulp.task('style', function() {
       fix: true,
     }))
     .pipe(gulp.dest('./'));
+
+});
+
+gulp.task('bundle', [ 'compile' ], function () {
+
+  var b = browserify({
+    entries: './lib/fetch.js',
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source('./fetch.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/'));
 
 });
 
