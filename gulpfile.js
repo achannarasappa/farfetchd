69a1496281e000cf99b0dd5d88f6130a1dabab91
Ani <ani@channarasappa.com>
2016-03-13 20:40:10 -0400

    Moved express server start and stop to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,10 @@ var Karma = require('karma').Server;
 var spawn = require('child_process').spawn;
 var runSequence = require('run-sequence');
 var mockServer = require('mockserver-grunt');
+var express = require('express');
+var zlib = require('zlib');
 var configuration = require('./test/configuration')
+var server;
 
 gulp.task('default', function() {
 });
@@ -69,6 +72,59 @@ gulp.task('server-mockserver-stop', function() {
 
 })
 
+gulp.task('server-express-start', function() {
+
+  var gzippedBody = zlib.gzipSync('test compressed body');
+  var redirect = function(count) {
+
+    return function(req, res) {
+
+      return res.redirect('/redirect_' + (count + 1))
+
+    };
+
+  };
+  var app = express();
+
+  // Redirect endpoints
+  app.get('/redirect_1', redirect(1));
+  app.get('/redirect_2', redirect(2));
+  app.get('/redirect_3', redirect(3));
+  app.get('/redirect_4', function(req, res) {
+
+    res.send('end redirect');
+
+  });
+
+  // Compression endpoint
+  app.get('/compression', function(req, res) {
+
+    res.set('Content-Encoding', 'gzip,deflate');
+    res.send(gzippedBody);
+
+  });
+
+  // Timeout endpoint
+  app.get('/timeout', function(req, res) {
+
+    setTimeout(function() {
+
+      return res.send('test timeout body')
+
+    }, 5000);
+
+  });
+
+  server = app.listen(configuration.EXPRESS_SERVER_PORT);
+
+})
+
+gulp.task('server-express-stop', function() {
+
+  server.close();
+
+})
+
 gulp.task('test-unit-client', function(done) {
 
   new Karma({
@@ -79,7 +135,11 @@ gulp.task('test-unit-client', function(done) {
     preprocessors: {
       'test/unit/**/*.js': 'browserify',
     },
-  }, () => done()).start();
+  }, function() {
+
+    return done();
+
+  }).start();
 
 });
 
@@ -93,7 +153,11 @@ gulp.task('test-functional-client', function(done) {
     preprocessors: {
       'test/functional/**/*.js': 'browserify',
     },
-  }, () => done()).start();
+  }, function() {
+
+    return done();
+
+  }).start();
 
 });
 
@@ -118,10 +182,12 @@ gulp.task('test-functional-server', function() {
 gulp.task('test-functional', function(done) {
 
   runSequence(
+    'server-express-start',
     'server-mockserver-start',
-    'test-functional-client',
-    //'test-functional-server',
+    //'test-functional-client',
+    'test-functional-server',
     'server-mockserver-stop',
+    'server-express-stop',
     done
   )
 
